2bed2764c067e864c273e8565141338bb4bfdc3e
Atomic1Science <atomic1science@gmail.com>
2019-11-03 18:28:25 -0500

    added mpd conf and changed setup to incorporate that

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-rsync --exclude ".git/" --exclude ".gitmodules" --exclude "LICENSE" --exclude "README.md" --exclude "setup.sh" -avh . ~
+rsync --exclude ".git/" --exclude ".gitmodules" --exclude "LICENSE" --exclude "README.md" --exclude "setup.sh" --exclude "mpd.conf" -avh . ~
+
+# mpd.conf is put in /etc/ which is obviously outside of ~, which is why it's excluded
 
