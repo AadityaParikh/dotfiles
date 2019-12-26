aa9fd09d0db072c5551d1748ea4477fe3bb7733e
Atomic1Science <atomic1science@gmail.com>
2019-12-26 06:18:26 -0500

    music change script changed

--- .scripts/musicchange.sh
+++ .scripts/musicchange.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mpc add /
-mpc -f %title% listall | rofi -dmenu -p "song" | xargs -I % mpc searchplay %
+mpc -f %title% listall | rofi -dmenu -p "song" | xargs -I % mpc searchplay title %
 mpc | tail -n 1 | xargs -I % notify-send '%'
