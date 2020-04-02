cde426a9813e0afde70c8564dab3180b8b959025
Atomic1Science <atomic1science@gmail.com>
2020-04-02 17:04:28 -0400

    changed terminal colors

--- .scripts/changeTerminalColors.sh
+++ .scripts/changeTerminalColors.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 if [ "$TERM" = "linux" ]; then
-	printf '\033]P01a1813'; # black
-	printf '\033]P1991f1f'; # red
-	printf '\033]P25c991f'; # green
-	printf '\033]P3997b1f'; # yellow
-	printf '\033]P41f3e99'; # blue
-	printf '\033]P5991f70'; # magenta
-	printf '\033]P61f9999'; # cyan
-	printf '\033]P7ccbc95'; # white
-	printf '\033]P8333026'; # brighter black
-	printf '\033]P9E62E2E'; # brighter red
-	printf '\033]PA8AE62E'; # brighter green
-	printf '\033]PBE6B82E'; # brighter yellow
-	printf '\033]PC2E5CE6'; # brighter blue
-	printf '\033]PDE62EA9'; # brighter magenta
-	printf '\033]PE2EE6E6'; # brighter cyan
-	printf '\033]PFE6D7AB'; # brighter white
-clear;
-fi;
+	/bin/echo -e "
+		\e]P02b303b
+		\e]P1bf616a
+		\e]P2a3be8c
+		\e]P3ebcb8b
+		\e]P48fa1b3
+		\e]P5b48ead
+		\e]P696b5b4
+		\e]P7c0c5ce
+		\e]P865737e
+		\e]P9bf616a
+		\e]PAa3be8c
+		\e]PBebcb8b
+		\e]PC8fa1b3
+		\e]PDb48ead
+		\e]PE96b5b4
+		\e]PFeff1f5
+	"
+	# get rid of artifacts
+	clear
+fi
