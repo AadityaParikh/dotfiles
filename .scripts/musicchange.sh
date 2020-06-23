3bc8ea147d1c4b9e3922f86442398b73de587faf
Atomic1Science <atomic1science@gmail.com>
2020-06-23 15:11:35 -0400

    changed script from changing track to meta actions

--- .scripts/musicchange.sh
+++ .scripts/musicchange.sh
@@ -1,4 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+#I'm sorry
 
 mpc add /
-mpc -f %title% listall | rofi -dmenu -p "song" | xargs -I % mpc searchplay title %
+choice="$(printf "Change Song\nPlay Toggle\nForward\nBackwards\nRestart" | rofi -dmenu -p "Music Controls")"
+
+
+case $choice in
+	"Change Song")
+		mpc -f %title% listall | rofi -dmenu -p "song" | xargs -I % mpc searchplay title %
+		;;
+	"Play Toggle")
+		mpc toggle
+		;;
+	"Forward")
+		mpc next
+		;;
+	"Backwards")
+		mpc prev
+		;;
+	"Restart")
+		mpc prev && mpc next
+		;;
+esac
