684a5d656a0ffb6e169c4bb006633bc1fb71d0ea
Atomic1Science <atomic1science@gmail.com>
2020-07-18 02:31:43 -0400

    xargs big dum

--- .scripts/clipInsert.sh
+++ .scripts/clipInsert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xclip -o -se c > /tmp/clipboard
 [ "$(file /tmp/clipboard | grep -e "ASCII" -e "UTF-8" -c )" = "0" ] && notify-send -i /tmp/clipboard Saved && exit
-printf "$(cat /tmp/clipboard)" | xargs -I{} notify-send "Saved" "{}"
+printf "$(cat /tmp/clipboard)" | xargs -I{} -0 notify-send "Saved" "{}"
 
