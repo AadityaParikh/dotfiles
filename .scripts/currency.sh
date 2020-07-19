3ce609d4812dcfbd9806532dffc9eb60f1fb8ce4
Atomic1Science <atomic1science@gmail.com>
2020-07-19 02:35:25 -0400

    made moving mouse harder

--- .scripts/currency.sh
+++ .scripts/currency.sh
@@ -2,4 +2,5 @@
 
 option=$(printf "USD_CAD\nCAD_USD" | sed 's/_/ to /g' | rofi -dmenu -p "conversion" | sed 's/ to /_/g')
 price=$(echo $option | xargs -I{} curl "https://free.currconv.com/api/v7/convert?q={}&compact=ultra&apiKey=d826856594c27d92238c") | jq ".$option"
+echo $price for $option
 notify-send $(rofi -dmenu -p "price" | sed "s/$/\*$price/" | bc)
