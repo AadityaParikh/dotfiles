c1d3530ca104031935037e04692c6cd3287d09ae
Atomic1Science <atomic1science@gmail.com>
2020-09-10 23:50:16 -0400

    added listening by album or artist to musicchange

--- .scripts/musicchange.sh
+++ .scripts/musicchange.sh
@@ -2,9 +2,21 @@
 
 
 mpc add /
-choice="$(printf "Play Toggle\nChange Song\nInsert Song\nFind Song\nForward\nBackwards\nRestart\nHard Shuffle\nShuffle" | rofi -dmenu -i -p "Music Controls")"
+choice="$(printf "Play Toggle\n\
+Change Song\n\
+Insert Song\n\
+Find Song\n\
+Forward\n\
+Backwards\n\
+Restart\n\
+Hard Shuffle\n\
+Shuffle\n\
+List\n\
+Listen by Album\n\
+Listen by Artist\
+" | rofi -dmenu -i -p "Music Controls")"
 
-
+ARTIST=""
 case $choice in
 	"Change Song")
 		mpc -f %title% listall | rofi -dmenu -i -p "song" | xargs -I{} mpc find title {} | xargs -I{} mpc insert {} && mpc next
@@ -28,9 +40,21 @@ case $choice in
 		mpc prev && mpc next
 		;;
 	"Hard Shuffle")
-		mpc crop && mpc add /
+		mpc clear && mpc add /
 		;;
 	"Shuffle")
 		mpc shuffle
+		;;
+	"List")
+		notify-send "`mpc playlist`"
+		;;
+	"Listen by Album")
+		mpc clear
+		ARTIST=`mpc list artist | rofi -dmenu -p "Artist"`
+		mpc find album "`mpc -f %album% search artist $ARTIST | uniq | rofi -dmenu -p "Album"`" artist "$ARTIST" | mpc add
+		;;
+	"Listen by Artist")
+		mpc clear
+		mpc search artist "`mpc list artist | rofi -dmenu -p "Artist"`" | mpc add
 		;;
 esac
