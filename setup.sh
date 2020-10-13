2063bef28f7a91521ef05a19000970df76268cbf
AadityaParikh <aadi1parikh@gmail.com>
2020-10-12 23:32:42 -0400

    main theme is now stored as an Xresources file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync --exclude ".gitignore" --exclude ".git/" --exclude ".gitmodules" --exclude "LICENSE" --exclude "README.md" --exclude "setup.sh" --exclude "mpd.conf" -avh . ~
+rsync --exclude ".gitignore" --exclude ".git/" --exclude ".gitmodules" --exclude "LICENSE" --exclude "README.md" --exclude "setup.sh" --exclude "mpd.conf" --exclude "mainTheme" -avh . ~
 rsync mpd.conf /etc/mpd.conf
