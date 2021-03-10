81bbd3e83ebb50e13b57f72cedff36ceeada81b6
AadityaParikh <aadi1parikh@gmail.com>
2021-03-09 19:52:58 -0500

    changed mount script to exclude mounted fs

--- .scripts/mount.sh
+++ .scripts/mount.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-lsblk | tail --lines=+2 | grep part | grep -v sda | cut --complement -c -6 | fzf | cut -f 1 -d ' ' | xargs -I % sudo mount "/dev/%" "/mnt/usbdrive"
+lsblk | tail --lines=+2 | grep part | grep -v '/' | cut --complement -c -6 | fzf | cut -f 1 -d ' ' | xargs -I % sudo mount "/dev/%" "/mnt/usbdrive"
 
 lsblk -f
