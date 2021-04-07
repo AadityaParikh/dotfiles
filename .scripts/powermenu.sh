74a32e68291d8f28cc64acbc7190912d8420e10d
Atomic1Sci <atomic1science@gmail.com>
2021-04-06 21:43:17 -0400

    cleaned up powermenu script

--- .scripts/powermenu.sh
+++ .scripts/powermenu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Dependancies: i3 & jq
+#Dependancies: i3
 
 powerOff=""
 reboot=""
 lock=""
-options="$powerOff\n$reboot\n$lock"
+options="$lock\n$reboot\n$powerOff"
 
 chosen="$(echo -e "$options" | rofi -dmenu -p "" -selected-row 2)"
 case $chosen in
