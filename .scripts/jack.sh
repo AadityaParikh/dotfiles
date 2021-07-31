02246f8190f5941f297aa847177384730958150b
Atomic1Sci <atomic1science@gmail.com>
2021-07-31 03:26:59 -0400

    housekeeping

--- .scripts/jack.sh
+++ .scripts/jack.sh
@@ -3,6 +3,10 @@
 jackd --realtime --driver alsa --device hw:2,0 -n2 --period 1024 --rate 44100 &
 a2j_control --ehw && a2j_control --start
 
+pulseaudio -k
+pulseaudio --start
+mpd
+
 pactl load-module module-jack-sink channels=2
 pactl load-module module-jack-source channels=2
 pacmd set-default-sink jack_out
--- .scripts/mount.sh
+++ .scripts/mount.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-lsblk | tail --lines=+2 | grep part | grep -v '/' | cut --complement -c -6 | fzf | cut -f 1 -d ' ' | xargs -I % sudo mount "/dev/%" "/mnt/usbdrive"
+lsblk -r | tail --lines=+2 | grep part | grep -v '/' | fzf | cut -f 1 -d ' ' | xargs -I % sudo mount "/dev/%" "/mnt/usbdrive"
 
 lsblk -f
